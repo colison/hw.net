12a08842b3e5ebf5e4b1e2e42e119a6d9b1cc7c4
colison <engyingzn@gmail.com>
2020-09-07 19:03:06 +0800

    hw1

---
+++ 2.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a= { 2,6,7,4,8};
+            int sum = 0;
+            for(int b = 0; b < a.Length; b++)
+            {
+                sum += a[b];
+            }
+            double s = sum * 1.0 / a.Length;
+            Array.Sort(a);
+            Console.WriteLine("总和:" + sum + "平均：" + s + "最大值：" + a[a.Length - 1] + "最小值：" + a[0]);
+        }
+    }
+}
