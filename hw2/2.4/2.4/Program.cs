7bcd0827802efc595d4f5a33d69cdd0c8d764f80
colison <engyingzn@gmail.com>
2020-09-07 19:45:17 +0800

    hw1

---
+++ hw2/2.4/2.4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag=true;
+            int[,] a = { { 1, 2, 3, 6}, { 5, 1, 2, 3 } , { 9, 5, 1, 2 }  };
+            for(int  i = 0; i < a.GetLength(0); i++)
+       
+                for(int j = 0; j < a.GetLength(1); j++)
+
+                    if ((i + 1) < a.GetLength(0) && (j + 1) < a.GetLength(1) && a[i, j] != a[i + 1, j + 1])
+                        flag = false;
+          
+            Console.WriteLine(flag);
+        }
+    }
+}
