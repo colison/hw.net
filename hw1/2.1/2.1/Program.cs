84f9b52f7ab4699fc17095fe7b95b52464b7ebde
colison <engyingzn@gmail.com>
2020-09-09 14:28:31 +0800

    hw

---
+++ hw1/2.1/2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入指定数据");
+            int s = Int32.Parse(Console.ReadLine());
+            int a ;
+            Console.WriteLine("输出的质因数：");
+            for (a=2; a <= s; a++)
+            {
+                if (s % a == 0)
+                {
+                    Console.WriteLine(a+" ");
+                    while(s%a==0)
+                        s = s / a;
+                }
+            }
+        }
+    }
+}
---
+++ hw1/2.3/2.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 2;
+            int []num = new int[100];
+            for (int i = 0; i < 100; i++)
+                num[i] = i + 1;
+            for(index = 2; index <= 100; index++)
+            {
+                for(int i = 0; i < 100; i++)
+                {
+                    if (num[i]!=index && num[i] % index == 0)
+                        num[i] = 0;
+                }
+            }
+            for(int i = 0; i < 100; i++)
+            {
+                if (num[i] != 0)
+                    Console.WriteLine(num[i]);
+            }
+        }
+    }
+}
